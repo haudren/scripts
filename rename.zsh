eef7a25face8026039448449e66da5a4fae70b94
haudren <h.audren@gmail.com>
2013-09-12 14:35:33 +0900

    Added folder structure change and some output

--- rename.zsh
+++ rename.zsh
@@ -1,15 +1,37 @@
 #!/bin/zsh
+ARTIST=$(metaflac --show-tag=ARTIST $1 | sed -e 's/.*=//')
+ALBUM=$(metaflac --show-tag=ALBUM $1 | sed -e 's/.*=//')
 for file in $@
 do
+	TRACKNUMBER=$(metaflac --show-tag=TRACKNUMBER $file | sed -e 's/.*=//')
 	TITLE=$(metaflac --show-tag=TITLE $file | sed -e 's/.*=//')
-	NUM=$(metaflac --show-tag=TRACKNUMBER $file | sed -e 's/.*=//')
-	if [ "$NUM" -lt "10" ]
+
+	if [ $TRACKNUMBER -ge 10 ]
 	then
-		NEWNUM=0$NUM
+		FILENAME=$TRACKNUMBER' - '$TITLE'.flac'
 	else
-		NEWNUM=$NUM
+		FILENAME='0'$TRACKNUMBER' - '$TITLE'.flac'
 	fi
-	NEWFILE=$NEWNUM" - "$TITLE".flac"
-	mv $file $NEWFILE
-	echo $1" ==> "$NEWFILE
+	
+	if [ $file != $FILENAME ]
+	then
+		echo $file' --> '$FILENAME
+		mv -u $file $FILENAME
+	else
+		echo "No change for : "$file
+	fi	
 done
+FOLDER=$ARTIST' - '$ALBUM' [FLAC]'
+CURFOLDER=$(pwd | sed -e 's/.*\///')
+
+if [ $FOLDER != $CURFOLDER ]
+then 
+	echo $(pwd)' --> '$FOLDER
+	mkdir ../$FOLDER
+	mv * ../$FOLDER
+	cd ..
+	rm -r $CURFOLDER
+	cd $FOLDER
+else
+	echo "No folder stucture change"
+fi
