0da7a9fe0d72c5d7f698449033cee422a34ddee2
Hervé Audren <h.audren@gmail.com>
2014-06-04 13:46:09 +0900

    Now use message time for display

--- bag_compliance.py
+++ bag_compliance.py
@@ -4,7 +4,7 @@ import numpy
 import os
 import argparse
 
-joint = 18
+joint = 'HEAD_JOINT1'
 
 directory='/home/herve'
 bagfile = '2014-05-23-16-02-53.bag'
@@ -23,6 +23,7 @@ print [c.topic for c in bag._get_connections()]
 print "Reading bag"
 print "-Disable"
 disable_msgs = list(bag.read_messages(topics='/robot/controls/disable'))
+dit = [m[1].header.stamp.to_sec() for m in disable_msgs]
 
 error = [m[1].error\
         for m in disable_msgs]
@@ -33,18 +34,21 @@ error_dot = [m[1].error_dot\
 disable = [m[1].disable\
         for m in disable_msgs]
 
-print "-Actual"
-q = [m[1].joint_position
-    for m in bag.read_messages(topics='/robot/sensors/robot_state')]
+iq = bag.read_messages(topics='/robot/sensors/robot_state').next()[1].joint_name.index(joint)
+print "-Actual (#%s)" % iq
+q, qt = zip(*[(m[1].joint_position, m[1].header.stamp.to_time())
+    for m in bag.read_messages(topics='/robot/sensors/robot_state')])
 
-print "-Desired"
-qd = [m[1].position
-    for m in bag.read_messages(topics='/robot/controls/joint_states')]
+iqd = bag.read_messages(topics='/robot/controls/joint_states').next()[1].name.index(joint)
+print "-Desired (#%s)" % iqd
+qd, qdt = zip(*[(m[1].position, m[1].header.stamp.to_time())
+    for m in bag.read_messages(topics='/robot/controls/joint_states')])
+
 
 print "Post-treatment"
-energy = [e[joint]*ed[joint] for e, ed in zip(error, error_dot)]
+energy = [e[iqd+1]*ed[iqd+1] for e, ed in zip(error, error_dot)]
 
-jd = [d[joint] for d in disable]
+jd = [d[iqd+1] for d in disable]
 dt = max(max(jd), 1)
 et = min(min(jd), -1)
 disabling = [i for i, j in enumerate(jd) if j == dt]
@@ -54,18 +58,18 @@ print "Plotting"
 plt.figure(1)
 ax = plt.subplot(211)
 print "-Actual"
-ax.plot([e[joint-1] for e in q])
+ax.plot(qt, [e[iq] for e in q], 'b')
 print "-Desired"
-ax.plot([e[joint-1] for e in qd])
+ax.plot(qdt, [e[iqd] for e in qd], 'r')
 
-print "-Disable (%s)" % str(len(disabling))
+print "-Disable (%s+/%s-)" % (str(len(enabling)),str(len(disabling)))
 for d in disabling:
-  ax.axvspan(d, d+dt, facecolor='r', alpha=0.2)
+  ax.axvspan(dit[d], dit[d+int(dt)], facecolor='r', alpha=0.2)
 for e in enabling:
-  ax.axvspan(e, e-et, facecolor='g', alpha=0.2)
+  ax.axvspan(dit[e], dit[e-int(et)], facecolor='g', alpha=0.2)
 
 plt.subplot(212, sharex=ax)
 print "-Energy"
-plt.plot(energy)
+plt.plot(dit, energy)
 
 plt.show()
