22d6aac2d885e917076868ff701b35901fdedaaf
Hervé Audren <h.audren@gmail.com>
2014-07-02 10:42:29 +0900

    Added function to select correct wallpaper when switching from single to dual screen

--- xmonad.zsh
+++ xmonad.zsh
@@ -4,6 +4,27 @@ config='xmonad.hs'
 single='xmonad.hs'
 dual='xmonad_dual_screen.hs'
 
+function get_resolution
+{
+	dim=$(xdpyinfo | grep 'dimensions' | sed -e 's/\s*dimensions:\s*//' | sed -e 's/\s*pixels.*//')
+	resx=$(echo $dim | cut -d x -f 1)
+	resy=$(echo $dim | cut -d x -f 2)
+}
+
+function select_wallpaper
+{
+	get_resolution
+	wallpaper=$HOME/Pictures/wallpaper
+	res_wallpaper=$wallpaper"_"$resx"_"$resy
+	if [[ -f $res_wallpaper ]];
+	then
+		xloadimage -onroot -fullscreen $res_wallpaper
+	else
+		xloadimage -onroot -fullscreen $wallpaper
+	fi
+}
+
+
 function set_xmonad
 {
 	rm $HOME/.xmonad/$config
@@ -16,17 +37,17 @@ function restart_xmonad
 	xmonad --restart
 }
 
-function screen
+function scr_mode
 {
 	if [[ $1 == 'single' ]];
 	then
 		set_xmonad $single
-		restart_xmonad
 	fi
 
 	if [[ $1 == 'dual' ]];
 	then
 		set_xmonad $dual
-		restart_xmonad
 	fi
+	restart_xmonad
+	select_wallpaper
 }
