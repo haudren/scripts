5715ad8934c74e9a5cf5b664b8526b9fb768f8d9
Hervé Audren <h.audren@gmail.com>
2014-10-28 13:35:33 +0900

    New note taking system

---
+++ source/notes.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+function note
+{
+	$EDITOR $HOME/notes/$1.md
+}
+
+function notes
+{
+	ls $HOME/notes
+}
+
+function view
+{
+	inf=$HOME/notes/$1.md
+	outf=$HOME/notes/output/$1.html
+	echo "<link href=\"http://kevinburke.bitbucket.org/markdowncss/markdown.css\" rel=\"stylesheet\"></link>" > $outf
+	cmark $inf > $outf
+	firefox $outf
+}
