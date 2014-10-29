7aeab9cfd77b1e0763bb03268cf51d06a64e47e4
Hervé Audren <h.audren@gmail.com>
2014-10-29 11:30:34 +0900

    When looking for notes, display only note name

--- source/notes.zsh
+++ source/notes.zsh
@@ -7,13 +7,13 @@ function note
 
 function notes
 {
-	ls $HOME/notes
+	ls $HOME/notes | sed -e 's/\..*//'
 }
 
 function view
 {
 	inf=$HOME/notes/$1.md
-	outf=$HOME/notes/output/$1.html
+	outf=$HOME/notes/.output/$1.html
 	echo "<link href=\"http://kevinburke.bitbucket.org/markdowncss/markdown.css\" rel=\"stylesheet\"></link>" > $outf
 	cmark $inf > $outf
 	firefox $outf
