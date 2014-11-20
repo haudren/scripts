850bcb9c127914ebd849ce8addf407f9118a58fd
Hervé Audren <h.audren@gmail.com>
2014-11-20 17:09:47 +0900

    Actually make the note system work

--- source/notes.zsh
+++ source/notes.zsh
@@ -19,7 +19,8 @@ function view
 {
 	inf=$HOME/notes/$1.md
 	outf=$HOME/notes/.output/$1.html
-	echo "<link href=\"http://kevinburke.bitbucket.org/markdowncss/markdown.css\" rel=\"stylesheet\"></link>" > $outf
-	cmark $inf > $outf
+	echo "<meta charset=\"UTF-8\">" > $outf
+	echo "<link href=\"http://kevinburke.bitbucket.org/markdowncss/markdown.css\" rel=\"stylesheet\"></link>" >> $outf
+	cmark $inf >> $outf
 	firefox $outf
 }
