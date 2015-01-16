bdec05f212155e576f817dadea9e070a26923519
Hervé Audren <h.audren@gmail.com>
2015-01-16 17:44:35 +0900

    Added function to automatically configure/build projects using cmake + make

--- source/misc.zsh
+++ source/misc.zsh
@@ -4,3 +4,28 @@ function tex_src()
 {
 	apt-file -x search '/'$1'.sty$'
 }
+
+function build()
+{
+	if [[ ! -d build ]]
+	then
+		echo "Make dir"
+		mkdir build
+	fi
+
+	if [[ $1 == "debug" ]]
+	then
+		BUILDTYPE=Debug
+	elif [[ $1 == "release" ]]
+	then
+		BUILDTYPE=Release
+	else
+		echo "Please supply debug/release"
+		return 1
+	fi
+
+	cd build
+	cmake -DCMAKE_BUILD_TYPE=$BUILDTYPE ..
+	make
+	cd ..
+}
