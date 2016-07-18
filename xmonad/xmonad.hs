7435cdff932043b24a5ef829ce166faefde8823d
Hervé Audren <h.audren@gmail.com>
2016-07-18 15:17:29 +0900

    Navigate xmonad using vim setup

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,13 +8,14 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Fullscreen
 import System.IO
 import XMonad.Layout.IndependentScreens
+import XMonad.Layout.WindowNavigation
 import XMonad.Hooks.EwmhDesktops
 
 myWorkspaces = ["1:main","2:xp","3:py","4:mail"] ++ map show [5..9]
 
 myManageHooks = manageDocks <+> manageHook defaultConfig <+> composeAll [ isFullscreen --> doFullFloat  									,className =? "xcalendar" --> doFloat]
 
-myLayoutHooks = smartBorders $ avoidStruts ( tiled ||| Mirror tiled ||| noBorders (fullscreenFull Full))
+myLayoutHooks = windowNavigation $ smartBorders $ avoidStruts ( tiled ||| Mirror tiled ||| noBorders (fullscreenFull Full))
 	where
 		tiled = Tall nmaster delta ratio
 		nmaster = 1
@@ -34,7 +35,7 @@ main = do
 				},
 		modMask = mod4Mask
 		} `additionalKeys`
-		[ ((mod4Mask .|. shiftMask, xK_l), spawn "slock")
+		[ ((mod4Mask .|. shiftMask, xK_Escape), spawn "slock")
 		, ((0                     , 0x1008FF11), spawn "change_volume 2-")
 		, ((0                     , 0x1008FF13), spawn "change_volume 2+")
 		, ((0                     , 0x1008FF12), spawn "change_volume toggle")
@@ -42,4 +43,14 @@ main = do
 		, ((0                     , 0x1008FF16), spawn "mediacontrol prev")
 		, ((0                     , 0x1008FF17), spawn "mediacontrol next")
 		, ((0                     , xK_Print), spawn "scrot $HOME/Pictures/screen_%Y-%m-%d-%H-%M-%S.png -d 1")
+		, ((mod4Mask              , xK_l), sendMessage $ Go R)
+		, ((mod4Mask              , xK_h), sendMessage $ Go L)
+		, ((mod4Mask              , xK_k), sendMessage $ Go U)
+		, ((mod4Mask              , xK_j), sendMessage $ Go D)
+		, ((mod4Mask .|. shiftMask, xK_l), sendMessage $ Swap R)
+		, ((mod4Mask .|. shiftMask, xK_h), sendMessage $ Swap L)
+		, ((mod4Mask .|. shiftMask, xK_k), sendMessage $ Swap U)
+		, ((mod4Mask .|. shiftMask, xK_j), sendMessage $ Swap D)
+		, ((mod4Mask .|. shiftMask, xK_comma), sendMessage Shrink)
+		, ((mod4Mask .|. shiftMask, xK_period), sendMessage Expand)
 		]
--- xmonad/xmonad_dual_screen.hs
+++ xmonad/xmonad_dual_screen.hs
@@ -10,6 +10,7 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Fullscreen
 import System.IO
 import XMonad.Layout.IndependentScreens
+import XMonad.Layout.WindowNavigation
 import XMonad.Hooks.EwmhDesktops
 
 myWorkspaces = withScreens 2 $ ["1:main","2:xp","3:py","4:mail"] ++ map show [5..9]
@@ -17,7 +18,7 @@ myWorkspaces = withScreens 2 $ ["1:main","2:xp","3:py","4:mail"] ++ map show [5.
 myManageHooks = manageDocks <+> manageHook defaultConfig <+> composeAll [ isFullscreen --> doFullFloat
 									, className =? "xcalendar" --> doFloat]
 
-myLayoutHooks = smartBorders $ avoidStruts ( tiled ||| Mirror tiled ||| noBorders (fullscreenFull Full))
+myLayoutHooks = windowNavigation $ smartBorders $ avoidStruts ( tiled ||| Mirror tiled ||| noBorders (fullscreenFull Full))
 	where
 		tiled = Tall nmaster delta ratio
 		nmaster = 1
@@ -51,7 +52,7 @@ myKeys = [
 	      , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
 	 ]
 	  ++
-	[((mod4Mask .|. shiftMask, xK_l), spawn "slock")
+	[((mod4Mask .|. shiftMask, xK_Escape), spawn "slock")
 	, ((0                     , 0x1008FF11), spawn "change_volume 2-")
 	, ((0                     , 0x1008FF13), spawn "change_volume 2+")
 	, ((0                     , 0x1008FF12), spawn "change_volume toggle")
@@ -59,6 +60,16 @@ myKeys = [
 	, ((0                     , 0x1008FF16), spawn "mediacontrol prev")
 	, ((0                     , 0x1008FF17), spawn "mediacontrol next")
 	, ((0                     , xK_Print), spawn "scrot $HOME/Pictures/screen_%Y-%m-%d-%H-%M-%S.png -d 1")
+	, ((mod4Mask              , xK_l), sendMessage $ Go R)
+	, ((mod4Mask              , xK_h), sendMessage $ Go L)
+	, ((mod4Mask              , xK_k), sendMessage $ Go U)
+	, ((mod4Mask              , xK_j), sendMessage $ Go D)
+	, ((mod4Mask .|. shiftMask, xK_l), sendMessage $ Swap R)
+	, ((mod4Mask .|. shiftMask, xK_h), sendMessage $ Swap L)
+	, ((mod4Mask .|. shiftMask, xK_k), sendMessage $ Swap U)
+	, ((mod4Mask .|. shiftMask, xK_j), sendMessage $ Swap D)
+	, ((mod4Mask .|. shiftMask, xK_comma), sendMessage Shrink)
+	, ((mod4Mask .|. shiftMask, xK_period), sendMessage Expand)
 	]
 
 main = do
